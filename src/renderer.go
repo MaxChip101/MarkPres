859dcd43dfccf782a121aec203f1addcef2f4c9b
MaxChip <1Max0Lag1@gmail.com>
2025-04-28 14:34:36 -0400

    removed useless function

--- src/renderer.go
+++ src/renderer.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"golang.org/x/term"
-)
-
-func GetTerminalSize() (int, int, error) {
-	width, height, err := term.GetSize(0)
-	return width, height, err
-}
-
 /*
 this function takes a slide and renders on the terminal depending on the markdown ast in the slide
 returns nothing as it doesn't need to return anything
